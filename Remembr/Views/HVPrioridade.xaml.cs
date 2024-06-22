51ffd28fd6dd7fc8dc6516162fc821363344f362
João Esteves <joaoesteves10A4@sapo.pt>
2024-06-22 01:05:44 +0100

    view da prioridade custom

--- Remembr/Views/HVPrioridade.xaml.cs
+++ Remembr/Views/HVPrioridade.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Remembr.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,17 +13,55 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Remembr.ViewModels;
 
 namespace Remembr.Views
 {
     /// <summary>
     /// Interaction logic for HVPrioridade.xaml
     /// </summary>
+    /// 
     public partial class HVPrioridade : UserControl
     {
+        MainVM MVM;
+        HVNovaPrioridade? HVNovaPrioridade;
+
         public HVPrioridade()
         {
             InitializeComponent();
+            MVM = (MainVM)Application.Current.MainWindow.DataContext;
+            if (HVNovaPrioridade == null)
+            {
+                HVNovaPrioridade = new HVNovaPrioridade();
+                HVNovaPrioridade.DataContext = new HVNovaPrioridadeVM();
+            }
+
+
+        }
+
+        private void Prioridadex_Click(object sender, RoutedEventArgs e)
+        {
+            pcc.Content = HVNovaPrioridade;
+        }
+
+        private void Prioridade400_Click(object sender, RoutedEventArgs e)
+        {
+            pcc.Content = null;
+        }
+
+        private void Prioridade300_Click(object sender, RoutedEventArgs e)
+        {
+            pcc.Content = null;
+        }
+
+        private void Prioridade200_Click(object sender, RoutedEventArgs e)
+        {
+            pcc.Content = null;
+        }
+
+        private void Prioridade100_Click(object sender, RoutedEventArgs e)
+        {
+            pcc.Content = null;
         }
     }
 }
