8dc60300a08a25271f4211380cb05b073ece70be
João Esteves <joaoesteves10A4@sapo.pt>
2024-06-23 23:45:15 +0100

    botão de mover

--- Remembr/Views/HVTarefa.xaml.cs
+++ Remembr/Views/HVTarefa.xaml.cs
@@ -53,7 +53,6 @@ namespace Remembr.Views
                 data.Text = t.DataInicio.ToString("dd/MM/yyyy") + " das " + t.DataInicio.ToString("HH:mm") + " até às " + ((DateTime)t.DataFim).ToString("HH:mm");
             }
 
-
             if (new List<int> { 100, 200, 300, 400 }.Contains(t.valorPrio))
             {
                 switch (t.valorPrio)
@@ -108,6 +107,16 @@ namespace Remembr.Views
                 return;
             }
             border.BorderBrush = new BrushConverter().ConvertFromString(prio.Cor) as SolidColorBrush;
+
+            if (t.Estado == 0 || t.Estado == 1)
+            {
+                PassarTarefa.Visibility = Visibility.Visible;
+                PassarTarefa.IsEnabled = true;
+            }
+            else { 
+                PassarTarefa.Visibility = Visibility.Hidden;
+                PassarTarefa.IsEnabled = false;
+            }
 
 
         }
@@ -165,7 +174,31 @@ namespace Remembr.Views
 
         private void PassarTarefa_Click(object sender, RoutedEventArgs e)
         {
+            if (MVM == null)
+            {
+                MessageBox.Show("erro MVM");
+                return;
+            }
+            if (MVM.GTarefas == null)
+            {
+                MessageBox.Show("erro tarefas");
+                return;
+            }
 
+            if (task == null)
+            {
+                MessageBox.Show("erro tarefa");
+                return;
+            }
+
+            if (task.Estado == 0 || task.Estado == 1)
+            {
+                task.Estado += 1;
+            }
+
+            MVM.SaveTarefas();
+            gr.Visibility = Visibility.Hidden;
+            movida.Visibility = Visibility.Visible;
         }
     }
 }
